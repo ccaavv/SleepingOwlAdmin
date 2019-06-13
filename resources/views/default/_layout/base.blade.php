7e5d06094d2ef87c08fd065f18c339efb161c17d
Aleksand Tsybenko <cccaaavvv@gmail.com>
2019-06-13 17:49:53 +0300

    small fix

--- resources/views/default/_layout/base.blade.php
+++ resources/views/default/_layout/base.blade.php
@@ -1,20 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	{!! $template->renderMeta($title) !!}
+{!! $template->renderMeta($title) !!}
 
-	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 	<![endif]-->
-
 	@stack('scripts')
 </head>
-<body class="skin-red sidebar-mini">
-	@yield('content')
+<body class="skin-red sidebar-mini @if(session('sidebar-state') == 'true') sidebar-collapse @endif">
+@yield('content')
 
-	{!! $template->meta()->renderScripts(true) !!}
-	@stack('footer-scripts')
+{!! $template->meta()->renderScripts(true) !!}
+@stack('footer-scripts')
 </body>
 </html>
--- src/Navigation/Badge.php
+++ src/Navigation/Badge.php
@@ -1,53 +1,52 @@
 <?php
 
-namespace SleepingOwl\Admin\Navigation;
+	namespace SleepingOwl\Admin\Navigation;
 
-class Badge extends \KodiComponents\Navigation\Badge
-{
-    public $view = null;
+	class Badge extends \KodiComponents\Navigation\Badge
+	{
+		public $view = null;
 
-    /**
-     * @return array
-     */
-    public function toArray()
-    {
-        $value = $this->getValue();
+		/**
+		 * @return array
+		 */
+		public function toArray()
+		{
+			$value = $this->getValue();
+			if (!$this->hasClassProperty('label-', 'bg-')) {
+				$this->setHtmlAttribute('class', 'label-primary');
+			}
 
-        if (! $this->hasClassProperty('label-', 'bg-')) {
-            $this->setHtmlAttribute('class', 'label-primary');
-        }
+			return [
+				'value'      => $value,
+				'attributes' => $this->htmlAttributesToString(),
+			];
+		}
 
-        return [
-            'value'      => $value,
-            'attributes' => $this->htmlAttributesToString(),
-        ];
-    }
+		/**
+		 * @return null|string
+		 */
+		public function getView()
+		{
+			if ($this->view) {
+				return $this->view;
+			}
 
-    /**
-     * @return null|string
-     */
-    public function getView()
-    {
-        if ($this->view) {
-            return $this->view;
-        }
+			return '_partials.navigation.badge';
+		}
 
-        return '_partials.navigation.badge';
-    }
+		/**
+		 * @param $view
+		 */
+		public function setView($view)
+		{
+			$this->view = $view;
+		}
 
-    /**
-     * @param $view
-     */
-    public function setView($view)
-    {
-        $this->view = $view;
-    }
-
-    /**
-     * @return \Illuminate\View\View|\Illuminate\Contracts\View\Factory
-     */
-    public function render()
-    {
-        return app('sleeping_owl.template')->view($this->getView(), $this->toArray());
-    }
-}
+		/**
+		 * @return \Illuminate\View\View|\Illuminate\Contracts\View\Factory
+		 */
+		public function render($view = null)
+		{
+			return app('sleeping_owl.template')->view($this->getView(), $this->toArray());
+		}
+	}
